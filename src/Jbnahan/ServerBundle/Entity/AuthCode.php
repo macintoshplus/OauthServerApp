c3212a1310b688f816c7847ad3b6d01d222deea5
MacintoshPlus <jean-baptiste@nahan.fr>
2014-12-18 09:46:36 +0100

    End add class and config

--- src/Jbnahan/ServerBundle/Entity/AuthCode.php
+++ src/Jbnahan/ServerBundle/Entity/AuthCode.php
@@ -6,8 +6,77 @@ namespace Acme\DemoBundle\Entity;
 use FOS\OAuthServerBundle\Entity\AuthCode as BaseAuthCode;
 use Doctrine\ORM\Mapping as ORM;
 
-/**
- * @ORM\Entity
- */
 class AuthCode extends BaseAuthCode
-{}
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var \Jbnahan\ServerBundle\Entity\Client
+     */
+    protected $client;
+
+    /**
+     * @var \Jbnahan\ServerBundle\Entity\User
+     */
+    protected $user;
+
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set client
+     *
+     * @param \FOS\OAuthServerBundle\Model\ClientInterface $client
+     * @return AccessToken
+     */
+    public function setClient(\FOS\OAuthServerBundle\Model\ClientInterface $client = null)
+    {
+        $this->client = $client;
+
+        return $this;
+    }
+
+    /**
+     * Get client
+     *
+     * @return \Jbnahan\ServerBundle\Entity\Client
+     */
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    /**
+     * Set user
+     *
+     * @param \Symfony\Component\Security\Core\User\UserInterface $user
+     * @return AccessToken
+     */
+    public function setUser(\Symfony\Component\Security\Core\User\UserInterface $user = null)
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    /**
+     * Get user
+     *
+     * @return \Jbnahan\ServerBundle\Entity\User
+     */
+    public function getUser()
+    {
+        return $this->user;
+    }
+}
