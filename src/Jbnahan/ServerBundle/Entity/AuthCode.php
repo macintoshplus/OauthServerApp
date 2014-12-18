4ad49c7b09e794ab240f5bdacde1ef1e3bcc8007
MacintoshPlus <jean-baptiste@nahan.fr>
2014-12-18 09:53:55 +0100

    fix typo

--- src/Jbnahan/ServerBundle/Entity/AuthCode.php
+++ src/Jbnahan/ServerBundle/Entity/AuthCode.php
@@ -1,11 +1,13 @@
 <?php
-// src/Acme/DemoBundle/Entity/AuthCode.php
 
 namespace Acme\DemoBundle\Entity;
 
 use FOS\OAuthServerBundle\Entity\AuthCode as BaseAuthCode;
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * class
+ */
 class AuthCode extends BaseAuthCode
 {
     /**
--- src/Jbnahan/ServerBundle/Entity/RefreshToken.php
+++ src/Jbnahan/ServerBundle/Entity/RefreshToken.php
@@ -1,11 +1,13 @@
 <?php
-// src/Acme/DemoBundle/Entity/RefreshToken.php
 
 namespace Acme\DemoBundle\Entity;
 
 use FOS\OAuthServerBundle\Entity\RefreshToken as BaseRefreshToken;
 use Doctrine\ORM\Mapping as ORM;
 
+/**
+ * class
+ */
 class RefreshToken extends BaseRefreshToken
 {
     /**
